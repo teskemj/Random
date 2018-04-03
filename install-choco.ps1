e0a5e386a419bb951c9e410121286a92b979b8d9
teskemj <michael.teske@outlook.com>
2018-04-03 18:46:52 -0500

    Random stuff

---
+++ install-choco.ps1
@@ -0,0 +1,22 @@
+﻿function install-choco {
+    [cmdletbinding()]
+    param (
+        [string]$path
+
+    )
+
+    $url = "http://www.chocolatey.org/install.ps1"
+    $output = "$path\install.ps1"
+
+    if (test-path $path) {
+        Invoke-WebRequest -uri $url -OutFile $output
+    } else {
+        md $path
+        Invoke-WebRequest -uri $url -OutFile $output
+    
+    }
+    
+    sl $path
+    .\install.ps1
+        
+}
